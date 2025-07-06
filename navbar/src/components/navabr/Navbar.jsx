b07040d53dc4641a0713e402c3484c8b5c379517
Wilhem Wundt Maxime <wilhemmaxime97@gmail.com>
2025-07-06 17:48:54 -0300

    Vamos limpar o projeto padrão

---
+++ navbar/src/components/navabr/Navbar.jsx
@@ -0,0 +1,13 @@
+function Navbar() {
+  return (
+    <header>
+        <div className="box-logo">
+            <span>
+                BERMAX
+            </span>
+        </div>
+    </header>
+  )
+}
+
+export default Navbar
