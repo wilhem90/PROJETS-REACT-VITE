d47c4527d43ccb507cda025daafa77be2cc46414
Wilhem Wundt Maxime <wilhemmaxime97@gmail.com>
2025-07-07 00:21:39 -0300

    Criamos nosso navbar bem simple

--- navbar/src/components/navabr/Navbar.jsx
+++ navbar/src/components/navabr/Navbar.jsx
@@ -1,13 +1,36 @@
-function Navbar() {
-  return (
+import { useState } from 'react';
+import "./Navbar.css"
+
+function Navabr() {
+    const [indexActive, setIndexActive] = useState(0)
+    const items = [
+        {path: "#", text: "Home"},
+        {path: "#", text: "About"},
+        {path: "#", text: "Logout"},
+    ]
+
+   
+  return(
     <header>
-        <div className="box-logo">
-            <span>
-                BERMAX
-            </span>
+        <div className="logo-text">
+            <span>BERMAX GLOBAL</span>
         </div>
+
+        <nav>
+            <ul>
+                {items.map((item, index) => (
+                    <li key={index}>
+                        <a href={item.path} className={indexActive === index? "active": ""} onClick={() => {
+                            setIndexActive(index)
+                        }}>
+                            {item.text}
+                        </a>
+                    </li>
+                ))}
+            </ul>
+        </nav>
     </header>
-  )
+  );
 }
 
-export default Navbar
+export default Navabr;
